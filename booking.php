62b7cebc006faf05bdef681db1812df9142ca411
Rajkanwar Singh <rajkanwarsingh15@gmail.com>
2023-04-11 15:01:51 +0530

    first

--- booking.php
+++ booking.php
@@ -4,6 +4,8 @@ if(!isset($_SESSION['user_id'])){
     header('location: signin.php');
     exit;
 }
+// Calculate the minimum journey date (two days from today)
+$min_date = date('Y-m-d', strtotime('+2 days'));
 ?>
 
 <!DOCTYPE html>
@@ -13,30 +15,21 @@ if(!isset($_SESSION['user_id'])){
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<header>
-    <h1>Railway Reservation System</h1>
-</header>
-<nav>
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="booking.php">Book a Reservation</a></li>
-        <li><a href="view-seats.php">View Available/Booked Seats</a></li>
-        <li><a href="cancel.php">Cancel Reservation</a></li>
-        <li><a href="refund.php">Apply for Refund</a></li>
-        <li><a href="signout.php">Sign Out</a></li>
-    </ul>
-</nav>
+<?php include 'header.php'; ?>
 <main>
     <h2>Book a Reservation</h2>
-    <form method="post" action="process-booking.php">
-        <label for="train_number">Train Number:</label>
-        <input type="text" id="train_number" name="train_number" required><br><br>
+    <form method="post" action="chooseTrain.php">
         <label for="from_station">From Station:</label>
-        <input type="text" id="from_station" name="from_station" required><br><br>
+        <input type="text" id="from_station" name="from_station" onkeyup="showSuggestions(this.value, 'from')" required>
+        <div id="from_suggestions"></div><br><br>
+
         <label for="to_station">To Station:</label>
-        <input type="text" id="to_station" name="to_station" required><br><br>
+        <input type="text" id="to_station" name="to_station" onkeyup="showSuggestions(this.value, 'to')" required>
+        <div id="to_suggestions"></div><br><br>
+
         <label for="journey_date">Journey Date:</label>
-        <input type="date" id="journey_date" name="journey_date" required><br><br>
+        <input type="date" id="journey_date" name="journey_date" min="<?php echo $min_date; ?>" required><br><br>
+
         <label for="class">Class:</label>
         <select id="class" name="class" required>
             <option value="">Select Class</option>
@@ -44,11 +37,33 @@ if(!isset($_SESSION['user_id'])){
             <option value="First">First Class</option>
             <option value="Second">Second Class</option>
         </select><br><br>
+
         <input type="submit" value="Book Now">
     </form>
 </main>
-<footer>
-    <p>&copy; 2023 Railway Reservation System. All rights reserved.</p>
-</footer>
+
+<script>
+    function showSuggestions(str, type) {
+        if (str.length == 0) {
+            document.getElementById(type + "_suggestions").innerHTML = "";
+            return;
+        } else {
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById(type + "_suggestions").innerHTML = this.responseText;
+                }
+            };
+            xmlhttp.open("GET", "get_suggestions.php?q=" + str + "&type=" + type, true);
+            xmlhttp.send();
+        }
+    }
+
+    function selectSuggestion(name, type) {
+        document.getElementById(type + "_station").value = name;
+        document.getElementById(type + "_suggestions").innerHTML = "";
+    }
+</script>
+<?php include 'footer.php'; ?>
 </body>
 </html>
